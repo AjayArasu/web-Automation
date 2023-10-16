046e5848f54bcf93605b47ae88b1b823c05668c4
pando-ajay <pando-ajay@4XYVKL3>
2023-10-16 18:06:00 +0530

    First commit

---
+++ codeleet/src/test/java/Testing/flipkart.java
@@ -0,0 +1,63 @@
+package Testing;
+
+import static org.testng.Assert.assertEquals;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class flipkart {
+	public static void main(String[] args) throws AWTException, IOException {
+		System.setProperty("webdriver.chrome.driver", "/home/pando-ajay/Documents/chromedriver_linux64/chromedriver");
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("--disable-notifications");
+		ChromeDriver driver = new ChromeDriver(opt);
+		WebDriverWait wait = new WebDriverWait(driver, 25);
+		Actions act = new Actions(driver);
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		driver.get("https://www.myntra.com");
+		driver.manage().window().maximize();
+//		driver.navigate().back();
+//		driver.navigate().forward();
+//		driver.findElement(By.xpath("//span[@class='_30XB9F']")).click();	
+//		driver.findElement(By.xpath("//div[@class='_2GaeWJ']")).click();
+		WebElement mens = driver.findElement(By.xpath("(//a[text()='Men'])[1]"));
+		act.moveToElement(mens).perform();
+//		driver.findElement(By.xpath("//span[title='T-Shirts]")).click();
+		WebElement tshirt = driver.findElement(By.xpath("(//a[text()='T-Shirts'])[1]"));
+		wait.until(ExpectedConditions.visibilityOf(tshirt)).click();
+//		Robot rob = new Robot();
+//		rob.keyPress(KeyEvent.VK_DOWN);
+		driver.findElement(By.xpath("(//li[@class='product-base']/a)[1]")).click();
+		Set<String> Handles = driver.getWindowHandles();
+		List<String> ls = new ArrayList<String>(Handles);
+		driver.switchTo().window(ls.get(1));
+		WebElement seller = driver.findElement(By.xpath("//span[contains(text(),'AHP APPAREL PRIVATE LIMITED - SJIT')]"));
+		js.executeScript("arguments[0].scrollIntoView(true)", seller);
+		js.executeScript("arguments[0].click();", seller );
+		File frst = driver.getScreenshotAs(OutputType.FILE);
+		File dest = new File("/home/pando-ajay/Documents/snaps/image.png");
+		FileHandler.copy(frst, dest);
+		///span[@class='supplier-productSellerName']
+		
+		}
+}
---
+++ codeleet/src/test/java/codeleet/login.java
@@ -0,0 +1,46 @@
+package codeleet;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.testng.Assert;
+
+public class login {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/home/pando-ajay/Documents/chromedriver_linux64/chromedriver");
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("--disable-notification");
+		WebDriver driver = new ChromeDriver(opt);
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		Actions act = new Actions(driver);
+		driver.get("https://leetcode.com/accounts/login/");
+		WebElement login = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//input[@class='input__2o8B '])[1]")));
+//		WebElement login = driver.findElement(By.xpath("(//input[@class='input__2o8B '])[1]"));
+		wait.until(ExpectedConditions.visibilityOf(login));
+		login.sendKeys("ajayvictor");
+		WebElement pass = driver.findElement(By.xpath("(//input[@class='input__2o8B '])[2]"));
+		pass.sendKeys("Open@123");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		WebElement loginbutton = driver.findElement(By.xpath("//button[@id='signin_btn']"));
+		js.executeScript("arguments[0].click();", loginbutton);
+//		WebElement until = wait.until(ExpectedConditions.elementToBeClickable(loginbutton));
+//		act.moveToElement(until).click();
+//		String explore = driver.findElement(By.xpath("//a[text()='Explore']")).getText();
+//		System.out.println(explore);
+//		Assert.assertEquals(explore, "Explore");
+//		System.out.println("finished");
+		System.out.println("login");
+		
+
+	
+	}
+	
+}
